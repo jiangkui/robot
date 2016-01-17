fd3c8f916216d84ec743fb01d56818cb262a4f8f
jiangkui <1013939150@qq.com>
2016-01-17 15:36:52 +0800

    新增性能测试代码--内存

--- src/main/java/com/ljkdream/core/controller/TestScheduleController.java
+++ src/main/java/com/ljkdream/core/controller/TestScheduleController.java
@@ -1,8 +1,17 @@
 package com.ljkdream.core.controller;
 
+import com.ljkdream.core.entity.UnifiedResponse;
 import com.ljkdream.proxy.service.ProxyServiceIpAddressService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 测试任务计划
@@ -10,6 +19,8 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class TestScheduleController {
+
+    private Logger logger = LoggerFactory.getLogger(TestScheduleController.class);
 
     @Autowired
     private ProxyServiceIpAddressService proxyServiceIpAddressService;
@@ -27,4 +38,23 @@ public class TestScheduleController {
 //
 //        return new UnifiedResponse();
 //    }
+
+    @ResponseBody
+    @RequestMapping("fillHeap")
+    public UnifiedResponse fillHeap() throws InterruptedException {
+        List<OOMObject> list = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            TimeUnit.MILLISECONDS.sleep(50);
+            list.add(new OOMObject());
+            logger.info("fill heap..." + i);
+        }
+        System.gc();
+        logger.info("内存回收完毕！");
+        return new UnifiedResponse();
+    }
+}
+
+class OOMObject {
+    public byte[] placeholder = new byte[64 * 1024];
 }
