ca2a75ced6b64f7ddd6cf6ac073d242486bfd57d
jiangkui <1013939150@qq.com>
2016-01-29 09:23:53 +0800

    测试

--- src/main/java/com/ljkdream/study/jconsole/WaitThreadTest.java
+++ src/main/java/com/ljkdream/study/jconsole/WaitThreadTest.java
@@ -45,11 +45,15 @@ public class WaitThreadTest {
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-        createBusyThread();
-        br.readLine();
-        createLockThread(new Object());
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        br.readLine();
+//        createBusyThread();
+//        br.readLine();
+//        createLockThread(new Object());
+
+        //java.lang.Runtime.availableProcessors() 方法返回到Java虚拟机的可用的处理器数量。此值可能会改变在一个特定的虚拟机调用。应用程序可用处理器的数量是敏感的，因此偶尔查询该属性，并适当地调整自己的资源使用情况.
+        int i = Runtime.getRuntime().availableProcessors();
+        System.out.println("i = " + i);
     }
 }
 
